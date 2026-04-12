39333ac6fd348c2f6e26534afd09ee636673e2c4
horoama <horoama929@gmail.com>
2026-04-12 15:15:30 +0900

    Fix some component

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,12 +1,20 @@
 import axios from 'axios';
 import { Platform } from 'react-native';
 
-// EXPO_PUBLIC_API_URL を環境変数から取得
-let API_URL = process.env.EXPO_PUBLIC_API_URL || 'http://localhost:3000/api';
+import Constants from 'expo-constants';
 
-// Webの場合は、現在のホスト名を使用してAPIのURLを動的に生成する
-if (!process.env.EXPO_PUBLIC_API_URL && Platform.OS === 'web' && typeof window !== 'undefined') {
-  API_URL = `http://${window.location.hostname}:3000/api`;
+// EXPO_PUBLIC_API_URL を環境変数から取得
+let API_URL = process.env.EXPO_PUBLIC_API_URL;
+
+if (!API_URL) {
+  if (Platform.OS === 'web' && typeof window !== 'undefined') {
+    API_URL = `http://${window.location.hostname}:3000/api`;
+  } else if (Constants.expoConfig?.hostUri) {
+    const host = Constants.expoConfig.hostUri.split(':')[0];
+    API_URL = `http://${host}:3000/api`;
+  } else {
+    API_URL = Platform.OS === 'android' ? 'http://10.0.2.2:3000/api' : 'http://localhost:3000/api';
+  }
 }
 
 export const api = axios.create({
